361f1c7d301b3542b5a6351382ad4e63ee11e793
kpotapovich <kris.potapovich@gmail.com>
2021-03-22 20:51:38 +0300

    feat: pointing to keys

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,67 +1,76 @@
-
 function playAudio(src) {
     const audio = new Audio();
     audio.src = src;
     audio.currentTime = 0;
     audio.play();
 }
-const key_a = document.getElementById("key_a");
-const url_a = 'assets/audio/a.mp3';
-key_a.addEventListener('click', () => playAudio(url_a))
-window.addEventListener('keydown', (event) => {
 
-
-});
+function addMouseEvents(key, url) {
+    key.addEventListener('mousedown', (event) => {
+        playAudio(url);
+        key.classList.add('piano-key-active');
+    });
+    key.addEventListener('mouseover', (event) => {
+        if (event.buttons === 1) {
+            playAudio(url);
+            key.classList.add('piano-key-active');
+        }
+    });
+    key.addEventListener('mouseup', (event) => {
+        key.classList.remove('piano-key-active');
+    });
+    key.addEventListener('mouseout', (event) => {
+        key.classList.remove('piano-key-active');
+    });
+}
 
 const key_c = document.getElementById("key_c");
 const url_c = 'assets/audio/c.mp3';
-key_c.addEventListener('click', () => playAudio(url_c))
+addMouseEvents(key_c, url_c);
 
 const key_d = document.getElementById("key_d");
 const url_d = 'assets/audio/d.mp3';
-key_d.addEventListener('click', () => playAudio(url_d))
+addMouseEvents(key_d, url_d);
 
 const key_e = document.getElementById("key_e");
 const url_e = 'assets/audio/e.mp3';
-key_e.addEventListener('click', () => playAudio(url_e))
+addMouseEvents(key_e, url_e);
 
 const key_f = document.getElementById("key_f");
 const url_f = 'assets/audio/f.mp3';
-key_f.addEventListener('click', () => playAudio(url_f))
+addMouseEvents(key_f, url_f);
 
 const key_g = document.getElementById("key_g");
 const url_g = 'assets/audio/g.mp3';
-key_g.addEventListener('click', () => playAudio(url_g))
+addMouseEvents(key_g, url_g);
+
+const key_a = document.getElementById("key_a");
+const url_a = 'assets/audio/a.mp3';
+addMouseEvents(key_a, url_a);
 
 const key_b = document.getElementById("key_b");
 const url_b = 'assets/audio/b.mp3';
-key_b.addEventListener('click', () => playAudio(url_b))
-
-
-
-
-
-
+addMouseEvents(key_b, url_b);
 
 const key_r = document.getElementById("key_r");
 const url_r = 'assets/audio/c♯.mp3';
-key_r.addEventListener('click', () => playAudio(url_r));
+addMouseEvents(key_r, url_r);
 
 const key_t = document.getElementById("key_t");
 const url_t = 'assets/audio/d♯.mp3';
-key_t.addEventListener('click', () => playAudio(url_t));
+addMouseEvents(key_t, url_t);
 
 const key_u = document.getElementById("key_u");
 const url_u = 'assets/audio/f♯.mp3';
-key_u.addEventListener('click', () => playAudio(url_u));
+addMouseEvents(key_u, url_u);
 
 const key_i = document.getElementById("key_i");
 const url_i = 'assets/audio/g♯.mp3';
-key_i.addEventListener('click', () => playAudio(url_i));
+addMouseEvents(key_i, url_i);
 
 const key_o = document.getElementById("key_o");
 const url_o = 'assets/audio/a♯.mp3';
-key_o.addEventListener('click', () => playAudio(url_o));
+addMouseEvents(key_o, url_o);
 
 
 
@@ -123,6 +132,7 @@ window.addEventListener('keyup', (event) => {
     switch (event.code) {
         case 'KeyD':
             key_c.classList.remove('piano-key-active');
+
             break;
         case 'KeyF':
             key_d.classList.remove('piano-key-active');
